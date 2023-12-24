7a36368e44cfea6b1cf6c32a63b3ee67ca837cfe
Anas <135501348+anasfanani@users.noreply.github.com>
2023-12-25 00:19:29 +0700

    delete internet availability check

--- service.sh
+++ service.sh
@@ -2,21 +2,10 @@
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
     sleep 1
 done
-# wait for network to be available
-echo "Waiting network to be available">/data/local/tmp/tailscaled.log
-while true; do
-    curl -Is 1.1.1.1 > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        break
-    else
-        echo "Network not available">>/data/local/tmp/tailscaled.log
-        sleep 2
-    fi
-done
 # ensure boot has actually completed & network is ready
 sleep 5
 # start tailscaled
-echo "Starting tailscaled">>/data/local/tmp/tailscaled.log
+echo "Starting tailscaled">/data/local/tmp/tailscaled.log
 while true;do
     # https://github.com/tailscale/tailscale/issues?q=no+safe+place+found+to+store+log+state
     cd /data/local/tmp/ && tailscaled -tun=userspace-networking -statedir=/data/local/tmp/ -state=/data/local/tmp/tailscaled.state -socket=/data/local/tmp/tailscaled.sock -port=41641 >> /data/local/tmp/tailscaled.log 2>&1
