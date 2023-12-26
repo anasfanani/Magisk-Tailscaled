cb267c2a651f49a4310856e51f084f7d401e2d8b
Anas <135501348+anasfanani@users.noreply.github.com>
2023-12-27 03:21:44 +0700

    Improvements for tailscale/d
    * Improvements for `tailscale` & `tailscaled` command.
    * Changed `/data/local/tmp/` to `/data/adb/tailscale/` & Refactor variable name.

--- customize.sh
+++ customize.sh
@@ -5,43 +5,45 @@ case $ARCH in
 esac
 
 ui_print "- Detected architecture: $F_ARCH"
-ui_print "- Extracting module files"
+ui_print "- Copying module files"
 
-F_TARGETDIR="$MODPATH/system/bin"
+CUSTOM_DIR="/data/adb/tailscale"
+CUSTOM_BIN_DIR="$CUSTOM_DIR/bin"
+CUSTOM_TMP_DIR="$CUSTOM_DIR/tmp"
 
-mkdir -p "$F_TARGETDIR"
-unzip -qq -o "$ZIPFILE" "files/tailscaled-$F_ARCH" -j -d "$F_TARGETDIR"
-unzip -qq -o "$ZIPFILE" "files/tailscale-$F_ARCH" -j -d "$F_TARGETDIR"
+mkdir -p "$CUSTOM_DIR" "$CUSTOM_BIN_DIR" "$CUSTOM_TMP_DIR"
 
-mv "$F_TARGETDIR/tailscaled-$F_ARCH" "$F_TARGETDIR/tailscaled"
-mv "$F_TARGETDIR/tailscale-$F_ARCH" "$F_TARGETDIR/tailscale"
-
-ui_print "- Extracted files to $F_TARGETDIR"
+cp "$MODPATH/tailscale/"* "$CUSTOM_DIR"
+cp "$MODPATH/files/tailscaled-$F_ARCH" "$CUSTOM_BIN_DIR/tailscaled"
+cp "$MODPATH/files/tailscale-$F_ARCH" "$CUSTOM_BIN_DIR/tailscale"
 
 ui_print "- Setting permissions"
 
-#set_perm_recursive $F_TARGETDIR 0 2000 0644 0755
-set_perm $MODPATH/system/bin/tailscale 0 0 0755
-set_perm $MODPATH/system/bin/tailscaled 0 0 0755
+# I wont hardcode the binary names here, because I will add other binaries in the future
+
+set_perm_recursive $CUSTOM_BIN_DIR 0 0 0755 0755
+set_perm_recursive $MODPATH/system/bin 0 0 0755 0755 
+
+if [ -f "/data/local/tmp/tailscaled.state" ]; then
+    ui_print "- Moving existing state file"
+    cp "/data/local/tmp/tailscaled.state" "$CUSTOM_TMP_DIR/tailscaled.state"
+    rm -f /data/local/tmp/tailscale* # cleanup old files
+fi
 
 ui_print "- Cleaning up"
-
-rm -r "$MODPATH/files" # cleanup
+rm -r "$MODPATH/files"
+rm -r "$MODPATH/tailscale"
 
 ui_print "*******************"
 ui_print " Instructions       "
 ui_print "*******************"
 ui_print "1. Reboot your device."
-ui_print "2. Open the Teminal and type 'cd /data/local/tmp/'."
-ui_print "3. Run 'su' to gain root access."
-ui_print "4. Run 'tailscale login' to login to your Tailscale account."
-ui_print "5. Open url in browser to authorize your device."
-ui_print "6. Run 'tailscale status' to check Tailscale connection."
+ui_print "2. Run 'su' to gain root access."
+ui_print "3. Run 'tailscale login' to login to your Tailscale account."
+ui_print "4. Open url in browser to authorize your device."
+ui_print "5. Run 'tailscale status' to check Tailscale connection."
 ui_print "*******************"
 ui_print " Troubleshooting    "
 ui_print "*******************"
-ui_print "1. Check logs in /data/local/tmp/tailscaled.log."
+ui_print "1. Check logs in /data/adb/tailscale/tmp/tailscaled.log."
 ui_print "2. Read the docs."
-
-
-
--- service.sh
+++ service.sh
@@ -5,10 +5,10 @@ done
 # ensure boot has actually completed & network is ready
 sleep 5
 # start tailscaled
-echo "Starting tailscaled">/data/local/tmp/tailscaled.log
+echo "Starting tailscaled">/data/adb/tailscale/tmp/tailscaled.log
 while true;do
-    # https://github.com/tailscale/tailscale/issues?q=no+safe+place+found+to+store+log+state
-    cd /data/local/tmp/ && tailscaled -tun=userspace-networking -statedir=/data/local/tmp/ -state=/data/local/tmp/tailscaled.state -socket=/data/local/tmp/tailscaled.sock -port=41641 >> /data/local/tmp/tailscaled.log 2>&1
+    # https://github.com/tailscale/tailscale/issues?q=no+safe+place+found+to+store+log+state 
+    cd /data/adb/tailscale/tmp/ && tailscaled >> /data/adb/tailscale/tmp/tailscaled.log 2>&1
     sleep 5
-    echo "Tailscaled restarting">>/data/local/tmp/tailscaled.log
+    echo "Tailscaled restarting">>/data/adb/tailscale/tmp/tailscaled.log
 done
