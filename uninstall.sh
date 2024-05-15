a538004706ee91cd4cdde3cf962c0cc8fff676d3
Anas <135501348+anasfanani@users.noreply.github.com>
2024-05-15 08:47:46 +0000

    fix kernelsu

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,5 @@
 rm -rf /data/adb/tailscale
 SERVICE_DIR="/data/adb/service.d"
-if [ "$KSU" = true ]; then
-  SERVICE_DIR="/data/adb/ksu/service.d"
-fi
 if [ -f "$SERVICE_DIR/tailscaled_service.sh" ]; then
     rm -f "$SERVICE_DIR/tailscaled_service.sh"
 fi
